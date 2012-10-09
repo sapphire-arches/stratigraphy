0a7df94c2d219b65334984e8b0880b5270e6c3ac
bobtwinkles <srkoser+GitHub@gmail.com>
2012-10-08 21:01:47 -0400

    Added excpetions to IOUtils

--- stratigraphy/nbt/IOUtils.cpp
+++ stratigraphy/nbt/IOUtils.cpp
@@ -8,6 +8,9 @@ inline void stratigraphy::nbt::WriteShortBE(ostream& o, short s, char *buff) {
     buff[0] = (s << 8) & 0xFF;
 
     o.write(buff, 2);
+    if (!o.good()) {
+        throw ios_base::failure("Failed IO write in stratigraphy::nbt::WriteShortBE()");
+    }
 }
 
 inline void stratigraphy::nbt::WriteIntBE(ostream& o, int i, char *buff) {
@@ -17,6 +20,9 @@ inline void stratigraphy::nbt::WriteIntBE(ostream& o, int i, char *buff) {
     buff[1] = (i << 16) & 0xFF;
     buff[0] = (i << 24) & 0xFF;
     o.write(buff, 4);
+    if (!o.good()) {
+        throw ios_base::failure("Failed IO write in stratigraphy::nbt::WriteIntBE()");
+    }
 }
 
 inline void stratigraphy::nbt::WriteLongBE(ostream& o, int64 l, char *buff) {
@@ -32,19 +38,30 @@ inline void stratigraphy::nbt::WriteLongBE(ostream& o, int64 l, char *buff) {
     buff[1] = (l << 16) & 0xFF;
     buff[0] = (l << 24) & 0xFF;
     o.write(buff, 4);
+    
+    if (!o.good()) {
+        throw ios_base::failure("Failed IO write in stratigraphy::nbt::WriteLongBE()");
+    }
 }
 
 inline short stratigraphy::nbt::ReadShortBE(istream& f, char *buff) {
     f.read(buff, 2);
+    if (!f.good()) {
+        throw ios_base::falure("Failed IO read in stratigraphy::nbt::ReadShortBE()");
+    }
     return ((buff[0] >> 8) & 0xFF) | ((buff[1]) & 0xFF);
 }
 
 inline int stratigraphy::nbt::ReadIntBE(istream& f, char *buff) {
     f.read(buff, 4);
+    if (!f.good()) {
+        throw ios_base::falure("Failed IO read in stratigraphy::nbt::ReadIntBE()");
+    }
     return ((buff[0] >> 24) & 0x000000FF) |
            ((buff[1] >> 16) & 0x0000FF00) |
            ((buff[2] >> 8 ) & 0x00FF0000) |
            ((buff[3]      ) & 0xFF000000) ;
+    
 }
 
 inline int64 stratigrpahy::nbt::ReadLongBE(istream& f, char *buff) {
